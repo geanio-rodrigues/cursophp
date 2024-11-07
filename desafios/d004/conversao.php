029edb5fd3743ea58d67d34e597997beb59ea7a8
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-11-07 17:18:06 -0300

    Atualização de Desafio
    Desafio refeito depois de aprender a puxar dados de uma API online.

--- desafios/d004/conversao.php
+++ desafios/d004/conversao.php
@@ -10,13 +10,24 @@
     <section>
         <h1>Conversor de Moedas v2.0</h1>
         <?php 
-            
+            // Pegando data atual e 7 dias antes, para puxar a cotação do dólar diretamente da API do banco central
+            $inicio = date("m-d-Y", strtotime("-7 dias"));
+            $fim = date("m-d-Y");
 
-            $cotacaoDolar = 5.76;
+            // Cotação vinda da API do Banco Central
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+            
+            $dados = json_decode(file_get_contents($url), true); // opção true, para colocar os dados em array
+            $cotacaoDolar = $dados["value"][0]["cotacaoCompra"];
             $real = $_GET["valor"];
-            $dolar = round($real / $cotacaoDolar, 2);
-            echo "<p>Seus R$ ".strtr($real, '.', ',')." equivalem a <strong>US$ ".strtr($dolar, '.', ',')."</strong></p>";
-            echo "<p><small><small>*Cotação obtida diretamente do site do <strong>Banco Central do Brasil</strong> </small></small></p>";
+            $dolar = $real / $cotacaoDolar;
+
+            // Criando um padrão para internacionalização de moedas
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+
+            echo "<p>Seus R$ ". numfmt_format_currency($padrao, $real, "BRL") ." equivalem a <strong>US$ ". numfmt_format_currency($padrao, $dolar, "USD") ."</strong>*</p>";
+            echo "<p><small><small>*Cotação: ". numfmt_format_currency($padrao, $cotacaoDolar, "BRL") ." obtida diretamente do site do <strong><a href='https://dadosaberts.bcb.gov.br'>Banco Central do Brasil</strong></a></small></small></p>";
         ?>
         <a href="javascript:history.go(-1)"><input type="button" value="Voltar"></a>
     </section>
