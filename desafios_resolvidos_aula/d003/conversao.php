04fda1436c8a0a6c8c7a453e3c67923052d470c5
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-11-07 16:21:39 -0300

    Atualização de exercício
    Resolução do desafio 3.

---
+++ desafios_resolvidos_aula/d003/conversao.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php 
+            // Cotação do dia, obtida no Google
+            $cotação = 5.69;
+
+            // Trazendo o valor do formulário
+            $real = $_REQUEST["din"];
+
+            // Transformando o valor obtido em real, para dólar
+            $dólar = $real / $cotação;
+
+            // Formatação de moedas com internacionalização
+            $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+            echo "<p> Seus ". numfmt_format_currency($padrão, $real, "BRL") ." equilavem à <strong>". numfmt_format_currency($padrão, $dólar, "USD") ."</strong></p>";
+        ?>
+        <button onclick="javascript:hitory.go(-1)">Voltar</button>
+    </main>
+</body>
+</html>
