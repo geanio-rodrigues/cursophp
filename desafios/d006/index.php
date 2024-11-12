0abf20201485af814f19e1abb81cf45d208c3170
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-11-12 15:28:19 -0300

    Resolução do Desafio 06
    Analisando uma divisão

---
+++ desafios/d006/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 6: Analisando uma divisão</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        $dividendo = $_POST["dividendo"] ?? 0;
+        $divisor = $_POST["divisor"] ?? 0;
+
+        $resto = $dividendo % $divisor;
+        $quociente = floor($dividendo / $divisor);
+    ?>
+    <main>
+        <h1>Anatomia de uma Divisão</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <label for="dividendo">Dividendo</label>
+            <input type="number" name="dividendo" id="dividendo" value="<?=$dividendo?>">
+            <label for="divisor">Divisor</label>
+            <input type="number" name="divisor" id="divisor" value="<?=$divisor?>">
+            <input type="submit" value="Analisar">
+        </form>
+    </main>
+    <section>
+        <h2>Estrutura da Divisão</h2>
+        <table>
+            <tr>
+                <td>Dividendo: <?=$dividendo?></td>
+                <td>Divisor: <?=$divisor?></td>
+            </tr>
+            <tr>
+                <td>Resto: <?=$resto?></td>
+                <td>Quociente: <?=$quociente?></td>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
