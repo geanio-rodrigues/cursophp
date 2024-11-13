4d0832eb79661b3228bf4337c868eacad6c10606
Geanio Rodrigues <geanio.rodrigues@hotmail.com>
2024-11-12 21:46:28 -0300

    Correção erro ortográfico

--- desafios/d009/index.php
+++ desafios/d009/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio 9: Médias Aritiméticas</title>
+    <title>Desafio 9: Médias Aritméticas</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -17,7 +17,7 @@
         $media_ponderada = ($valor1 * $peso1 + $valor2 * $peso2) / ($peso1 + $peso2);
     ?>
     <main>
-        <h2><big>Médias Aritiméticas</big></h2>
+        <h2><big>Médias Aritméticas</big></h2>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="v1">1º Valor</label>
             <input type="number" name="v1" id="v1" value="<?=$valor1?>">
